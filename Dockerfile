0c272f4cbf5756a8d321f0bc16161bb946d02283
Enijar <TheEnijar@gmail.com>
2021-11-11 09:13:13 +0000

    fix: aws cli install

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM alpine:3.14.2
 
 COPY entrypoint.sh /entrypoint.sh
 
-RUN apk --update add --no-cache bash mysql-client python3 py3-pip
-RUN pip3 install --upgrade pip
-RUN pip3 install --no-cache-dir awscli
-RUN awscli
+RUN apk --update add --no-cache bash mysql-client curl unzip
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/awscliv2.zip"
+RUN unzip /awscliv2.zip
+RUN /aws/install -i /usr/local/aws -b /usr/local/bin/aws
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ IGNORED_DATABASES=(Database mysql information_schema performance_schema cond_ins
 DATABASE_NAMES=($(echo "show databases;" | mysql -h "$DB_HOST" -p"$DB_PASSWORD" -u "$DB_USER"))
 TMP_DIR=/tmp
 
-awscli configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-awscli configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
 for i in "${DATABASE_NAMES[@]}"; do
   if [[ " ${IGNORED_DATABASES[@]} " =~ " ${i} " ]]; then
@@ -22,7 +22,7 @@ for i in "${DATABASE_NAMES[@]}"; do
     set echo on
 
     printf "Uploading to S3...\n"
-    awscli s3 cp "$TMP_DIR/$i-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$i-$FILENAME"
+    aws s3 cp "$TMP_DIR/$i-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$i-$FILENAME"
     printf "Uploaded to S3.\n"
   fi
 done
