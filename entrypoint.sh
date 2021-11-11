b30357cc4e7d28cba65bd071deb02f1d18ae8bd0
Enijar <TheEnijar@gmail.com>
2021-11-11 13:36:37 +0000

    feat: backup specific databases and specify ignored databases

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,35 @@
 
 TIME=$(date +%b-%d-%y-%H%M)
 FILENAME="backup-$TIME.tar.gz"
-
-IGNORED_DATABASES=(Database mysql information_schema performance_schema cond_instances)
-DATABASE_NAMES=($(echo "show databases;" | mysql -h "$DB_HOST" -p"$DB_PASSWORD" -u "$DB_USER"))
 TMP_DIR=/tmp
 
-aws --version
+DATABASES_TOTAL=${#DATABASES[@]}
+
+function backupDatabase() {
+  database=$1
+  if [[ " ${IGNORED_DATABASES[@]} " =~ " ${database} " ]]; then
+    printf "$database ignored\n"
+  else
+    printf "Backing up $database\n"
+
+    set echo off
+    mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$database" >"$TMP_DIR/$database.sql"
+    tar -cpzf "$TMP_DIR/$database-$FILENAME" "$TMP_DIR/$database.sql"
+    set echo on
+
+    printf "Uploading to S3...\n"
+    aws s3 cp "$TMP_DIR/$database-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$database-$FILENAME"
+    printf "Uploaded to S3.\n"
+  fi
+}
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
-for i in "${DATABASE_NAMES[@]}"; do
-  if [[ " ${IGNORED_DATABASES[@]} " =~ " ${i} " ]]; then
-    printf "$i ignored\n"
-  else
-    printf "Backing up $i\n"
+if [[ $DATABASES_TOTAL -eq 0 ]]; then
+  DATABASES=$(echo "show databases;" | mysql -h "$DB_HOST" -p"$DB_PASSWORD" -u "$DB_USER")
+fi
 
-    set echo off
-    mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$i" >"$TMP_DIR/$i.sql"
-    tar -cpzf "$TMP_DIR/$i-$FILENAME" "$TMP_DIR/$i.sql"
-    set echo on
-
-    printf "Uploading to S3...\n"
-    aws s3 cp "$TMP_DIR/$i-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$i-$FILENAME"
-    printf "Uploaded to S3.\n"
-  fi
+for i in ${DATABASES[@]}; do
+  backupDatabase $i
 done
